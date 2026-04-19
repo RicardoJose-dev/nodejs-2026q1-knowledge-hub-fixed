88781aa291f633b0fad6d7afbc6801234f67d792
c-ricardo-gomez <c-ricardo.gomez@hulu.com>
2026-04-19 10:30:28 -0500

    feat: implement guards

---
+++ src/common/guards/token.guard.ts
@@ -0,0 +1,28 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+
+@Injectable()
+export class TokenGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const authHeader =
+      request.headers['authorization'] || request.headers['Authorization'];
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      throw new UnauthorizedException(
+        'Missing or invalid Authorization header',
+      );
+    }
+
+    const token = authHeader.split(' ')[1];
+    if (!token) {
+      throw new UnauthorizedException('Token not found');
+    }
+
+    return true;
+  }
+}
